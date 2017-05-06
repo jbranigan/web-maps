edd7c0e491f12261227515c481d91c1e54df8db0
John Branigan <jb@ulysses.attlocal.net>
2017-05-06 12:54:32 -0500

    Add styles, vice map

--- simple-location-maps/js/map.js
+++ simple-location-maps/js/map.js
@@ -1,10 +1,47 @@
-var viceGlobeContext = d3.select("vice-globe").node().getContext("2d"),
-    path = d3.geoPath(d3.geoOrthographic(), context);
+var viceGlobe = function() {
+    var svg = d3.select('#vice-globe');
+    var dim = svg.style('width').replace("px", "") / 2;
 
-d3.json("https://unpkg.com/world-atlas@1/world/110m.json", function(error, world) {
-  if (error) throw error;
+    var path = d3.geoPath(d3.geoOrthographic()
+    .scale(dim)
+    .rotate([-100, -30])
+    .translate([dim, dim])
+    .clipAngle(90));
 
-  viceGlobeContext.beginPath();
-  path(topojson.mesh(world));
-  viceGlobeContext.stroke();
-});
+    d3.json('https://unpkg.com/world-atlas@1/world/110m.json', function(error, world) {
+    if (error) throw error;
+
+    svg.append('path')
+        .attr('stroke', '#333')
+        .attr('stroke-width', 4)
+        .attr('fill', 'none')
+        .attr('stroke-linejoin', 'round')
+        .attr('stroke-linecap', 'round')
+        .attr('d', path(topojson.mesh(world, world.objects.land)));
+    });
+};
+
+var nytWorld = function() {
+    var svg = d3.select('#nyt-world');
+    var dim = svg.style('width').replace("px", "") / 2;
+
+    var path = d3.geoPath(d3.geoOrthographic()
+    .scale(dim)
+    .rotate([60, -20])
+    .translate([dim, dim])
+    .clipAngle(90));
+
+    d3.json('https://unpkg.com/world-atlas@1/world/110m.json', function(error, world) {
+    if (error) throw error;
+
+    svg.append('path')
+        .attr('stroke', '#333')
+        .attr('stroke-width', 4)
+        .attr('fill', 'none')
+        .attr('stroke-linejoin', 'round')
+        .attr('stroke-linecap', 'round')
+        .attr('d', path(topojson.mesh(world, world.objects.land)));
+    });
+};
+
+viceGlobe();
